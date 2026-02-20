e653ffeface40784e2a22da5ca061c7b1051e099
ranya-tech <amzghar.ranya.solicode@gmail.com>
2026-02-20 10:00:12 +0000

    Chapitres

--- Chapitre 1 – Connexion à une base de données avec PDO/connexion.php
+++ Chapitre 1 – Connexion à une base de données avec PDO/connexion.php
@@ -7,7 +7,7 @@ $password = '';
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connexion réussie à la base $dbname";
+    echo "Connexion réussie à la base $dbname <br>";
 } catch (PDOException $e) {
     echo "Erreur de connexion : " . $e->getMessage();
 }
---
+++ Chapitre 2 – Exécuter une requête SELECT et afficher les résultats/liste_utilisateurs.php
@@ -0,0 +1,16 @@
+<?php
+require '../Chapitre 1 – Connexion à une base de données avec PDO/connexion.php';
+
+try {
+    $sql = "SELECT * FROM Utilisateur";
+    $stmt = $pdo->query($sql);
+
+    $utilisateurs = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($utilisateurs as $user) {
+        echo "ID : " . $user['id'] . " - Nom : " . $user['nom'] . " - Email : " . $user['email'] . "<br>";
+    }
+} catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+}
+?>
