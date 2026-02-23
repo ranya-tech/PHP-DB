8a53a9da9257136a54bfa9bdc27adfe0373707df
ranya-tech <amzghar.ranya.solicode@gmail.com>
2026-02-23 12:28:07 +0000

    Chapitres

--- Chapitre 2 – Exécuter une requête SELECT et afficher les résultats/liste_utilisateurs.php
+++ Chapitre 2 – Exécuter une requête SELECT et afficher les résultats/liste_utilisateurs.php
@@ -13,4 +13,10 @@ try {
 } catch (PDOException $e) {
     echo "Erreur : " . $e->getMessage();
 }
+echo "<table border='1'>
+<tr><th>ID</th><th>Nom</th><th>Email</th></tr>";
+foreach ($utilisateurs as $user) {
+    echo "<tr><td>{$user['id']}</td><td>{$user['nom']}</td><td>{$user['email']}</td></tr>";
+}
+echo "</table>";
 ?>
